bf3191f0b8f490547bc802da3f92f14aa4e72f24
Samantha Zenezi <samanthazenezi@gmail.com>
2022-10-17 19:17:07 -0300

    estilização rota login e mudança de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: "", component: HomeComponent },
+  { path: "home", component: HomeComponent },
   { path: "receita/:id", component: ReceitacompletaComponent },
   { path: "nova-receita", component: NovareceitaComponent },
-  { path: "login", component: LoginComponent }
+  { path: "", component: LoginComponent }
 ];
 
 @NgModule({
